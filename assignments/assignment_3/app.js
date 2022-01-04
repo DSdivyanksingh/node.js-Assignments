197b45bf9fb8f8733bff90aecd98d45e3a55b2f6
divyank12singh <divyanksingh1297@gmail.com>
2022-01-04 19:02:44 +0530

    Nodejs_appUsing_express

---
+++ assignments/assignment_3/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const faker = require('faker')
+const path=require("path")
+const app = express()
+const bodyparser = require('body-parser')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyparser.urlencoded({
+  extended : false
+}))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+var users = []
+for(let i=0; i<3; i++){
+  users.push({
+    name: faker.name.findName(),
+    email : faker.internet.email(),
+  })
+}
+app.get('/', (req, res) => {
+  res.render('index',{users})
+})
+
+app.get('/form', (req, res)=>{
+  res.render('form')
+})
+app.post('/user/add', (req, res)=>{
+  users.push({
+    name : req.body.names,
+    email : req.body.email
+  })
+  res.redirect('/')
+})
+
+app.listen(3000);
